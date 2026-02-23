2714107010d19a0eaa6abc48d8f8a5c681756aed
Igor Soarde de Luna <igorluna2006@gmail.com>
2026-02-23 17:04:48 -0300

    Descobrindo e manipulando variáveis de ambiente

---
+++ Fase 5 - Conhecendo Node.JS/nodeJS/09-npm-enviroment/src/database/data.js
@@ -0,0 +1,12 @@
+
+
+async function connectToDatabase(user,password){
+
+    if(user === process.env.USERDATABASE && password === process.env.PASSWORDDATABASE){ // Dado Exposto PROBLEMAAAA
+        console.log("conexão com banco de dados estabelecida!");
+    }else{
+        console.log("Falha de login, não foi possível se conectar ao banco de dados!");
+    }
+}
+
+export default connectToDatabase;
---
+++ Fase 5 - Conhecendo Node.JS/nodeJS/09-npm-enviroment/src/index.js
@@ -0,0 +1,9 @@
+import connectToDatabase from "./database/data.js";
+
+async function main(){
+
+    await connectToDatabase(process.env.USERDATABASE, process.env.PASSWORDDATABASE);
+
+}
+
+main();
