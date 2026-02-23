5a695fc3be8be7d6a2c79cde2da359f8824f7327
Igor Soarde de Luna <igorluna2006@gmail.com>
2026-02-23 16:59:43 -0300

    Manipulando pacotes e dependencias no nodeJS

---
+++ Fase 5 - Conhecendo Node.JS/nodeJS/07-npm-packages-basic/src/index.js
@@ -0,0 +1,7 @@
+import chalk from "chalk"; // Dependencia instalada
+import logSymbols from "log-symbols"; // Dev Dependencia
+
+
+console.log(chalk.green.bold("Hello World!"));
+
+console.log(logSymbols.success, "Servidor iniciado com sucesso!");
